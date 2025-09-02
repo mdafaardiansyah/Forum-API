c83f68f5501b7fbdeb46e08fb48c8ac8ef8bce82
Dafa Ardiansyah <ardidafa21@gmail.com>
2025-09-02 11:31:53 +0700

    feat(use_case): add AddCommentUseCase for handling comment creation
    Implement the use case to add comments with thread and user verification

---
+++ src/Applications/use_case/AddCommentUseCase.js
@@ -0,0 +1,24 @@
+const NewComment = require('../../Domains/comments/entities/NewComment');
+const AddedComment = require('../../Domains/comments/entities/AddedComment');
+
+class AddCommentUseCase {
+  constructor({ commentRepository, threadRepository, userRepository }) {
+    this._commentRepository = commentRepository;
+    this._threadRepository = threadRepository;
+    this._userRepository = userRepository;
+  }
+
+  async execute(useCasePayload) {
+    const newComment = new NewComment(useCasePayload);
+    
+    // Verify thread exists
+    await this._threadRepository.verifyThreadExists(newComment.threadId);
+    
+    // Verify user exists
+    await this._userRepository.verifyUserExists(newComment.owner);
+    
+    return this._commentRepository.addComment(newComment);
+  }
+}
+
+module.exports = AddCommentUseCase;
