1aeab05b77c93dec53c0fafdd3c52b9ff3102f45
Dafa Ardiansyah <ardidafa21@gmail.com>
2025-09-10 06:57:41 +0700

    refactor(AddReplyUseCase): improve code formatting and remove unused import
    - Format constructor parameters for better readability
    - Remove unused AddedReply import
    - Maintain consistent line endings

--- src/Applications/use_case/AddReplyUseCase.js
+++ src/Applications/use_case/AddReplyUseCase.js
@@ -1,8 +1,9 @@
 const NewReply = require('../../Domains/replies/entities/NewReply');
-const AddedReply = require('../../Domains/replies/entities/AddedReply');
 
 class AddReplyUseCase {
-  constructor({ replyRepository, commentRepository, threadRepository, userRepository }) {
+  constructor({
+    replyRepository, commentRepository, threadRepository, userRepository,
+  }) {
     this._replyRepository = replyRepository;
     this._commentRepository = commentRepository;
     this._threadRepository = threadRepository;
@@ -11,16 +12,16 @@ class AddReplyUseCase {
 
   async execute(useCasePayload) {
     const newReply = new NewReply(useCasePayload);
-    
+
     // Verify thread exists
     await this._threadRepository.verifyThreadExists(useCasePayload.threadId);
-    
+
     // Verify comment exists
     await this._commentRepository.verifyCommentExists(newReply.commentId);
-    
+
     // Verify user exists
     await this._userRepository.verifyUserExists(newReply.owner);
-    
+
     return this._replyRepository.addReply(newReply);
   }
 }
