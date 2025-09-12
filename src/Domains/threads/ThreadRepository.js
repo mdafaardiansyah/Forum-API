2c8d5e266810516fea018362b9bd7a640d99597a
Dafa Ardiansyah <ardidafa21@gmail.com>
2025-09-12 16:19:10 +0700

    refactor(ThreadRepository): add underscore prefix to unused parameters
    Standardize parameter naming convention by prefixing unused parameters with underscore to clearly indicate they are intentionally unused.

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -1,13 +1,13 @@
 class ThreadRepository {
-  async addThread(newThread) {
+  async addThread(_newThread) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 
-  async getThreadById(threadId) {
+  async getThreadById(_threadId) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 
-  async verifyThreadExists(threadId) {
+  async verifyThreadExists(_threadId) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 
@@ -15,7 +15,7 @@ class ThreadRepository {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 
-  async getDetailThreadById(threadId) {
+  async getDetailThreadById(_threadId) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 }
